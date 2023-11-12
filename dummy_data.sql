92b471f95845c6baab2b5abaf74680932ee59898
Oliver <113813346+oliverdela@users.noreply.github.com>
2023-11-12 14:23:41 +0800

    Update dummy_data.sql

--- dummy_data.sql
+++ dummy_data.sql
@@ -10,14 +10,30 @@ INSERT INTO course VALUES
 INSERT INTO attends VALUES
   (1, "COMP3278"),
   (2, "COMP3278"),
-  (2, "COMP3258");
+  (2, "COMP3258"),
+  (3, "COMP3278"),
+  (3, "ECON4211"),
+  (4, "COMP3278");
 INSERT INTO teacher_message VALUES
-  ("COMP3278", "20231101", "Remember to submit group project"),
-  ("COMP3258", "20231008", "Assignment 2 released");
+  ("COMP3278", "2023-11-01 14:00:23", "Remember to submit group project"),
+  ("COMP3258", "2023-10-08 08:59:35", "Assignment 2 released");
 INSERT INTO links_of_zoom VALUES
   ("COMP3278", "https://hku.zoom.us/j/98307568693?pwd=QmlqZERWeDdWRVZ3SGdqWG51YUtndz09");
-
-
+INSERT INTO tutorial_lecture_notes VALUES
+  (1, "COMP3278", "This is a dummy notes");
 INSERT INTO course_schedule VALUES
-  ("COMP3278", "143000", "152000", "Monday"),
-  ("COMP3278", "133000", "152000", "Thursday");
+  (1, "COMP3278", "143000", "152000", "Monday"),
+  (2, "COMP3278", "133000", "152000", "Thursday"),
+  (3, "COMP3258", "093000", "122000", "Friday"),
+  (4, "ECON4211", "143000", "172000", "Friday");
+INSERT INTO timetable VALUES
+  (1, 1, 1),
+  (2, 1, 2),
+  (3, 2, 1),
+  (4, 2, 2),
+  (5, 2, 3);
+INSERT INTO login_information VALUES
+  (1, 1, "2023-11-10 23:12:59", "2023-11-10 23:25:02", "Hello"),
+  (2, 4, "2023-11-10 23:12:59", "2023-11-10 23:25:02", "Hello");
+INSERT INTO face_recognition VALUES
+  (1, 1, "filepath");
