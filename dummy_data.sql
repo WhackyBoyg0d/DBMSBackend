e2379d46757e1455f224d260f495bd67ef135fb5
Oliver <113813346+oliverdela@users.noreply.github.com>
2023-11-12 13:37:01 +0800

    Create dummy_data.sql

---
+++ dummy_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO students VALUES
+  (1, "Aditya", "aditya42@connect.hku.hk"),
+  (2, "Aisan", "aazizi@connect.hku.hk"),
+  (3, "Oliver", "odelano@connect.hku.hk"),
+  (4, "Osman", "u3578258@connect.hku.hk");
+INSERT INTO course VALUES
+  ("COMP3278", "Introduction to Database Management Systems"),
+  ("COMP3258", "Functional Programming");
+INSERT INTO attends VALUES
+  (1, "COMP3278"),
+  (2, "COMP3278"),
+  (2, "COMP3258");
